d31104684d536495295a451c6956939e26582c7f
SahithiSiripuram <sahithisiripuram1001@gmail.com>
2020-12-05 02:51:13 +0530

    next commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,21 +1,23 @@
-var btnTranslate=document.querySelector('#btncolor');
-var txtInput=document.querySelector('#inputTxt');
-var txtOutput=document.querySelector('outputTxt');
-var serverURL="https://api.funtranslations.com/translate/minion.json";
-function getTranslation(text){
-    return serverURL+'?'+'text='+text;
+var btnTranslate = document.querySelector("#btncolor");
+var txtInput = document.querySelector("#inputTxt");
+var outputDiv = document.querySelector("#outputTxt");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
-function clickHandler(){
-    var inputText=txtInput.value;
-    fetch(getTranslation(inputText))
-        .then(response=>response.json())
-        .then(json=>{
-            var translatedTxt=json.contents.translated;
-            outputDiv.innerText=translatedTxt;;
-        })
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time. Maximum 5 outputs possible per hour")
+}
+function clickHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; 
+           })
         .catch(errorHandler)
 };
-function errorHandler(error){
-    alert('Something wrong with the server. Please try later. Maximum limit for the server is 5 requests per hour.')
-};
-btnTranslate.addEventListener("click",clickHandler)
+
+btnTranslate.addEventListener("click", clickHandler)
